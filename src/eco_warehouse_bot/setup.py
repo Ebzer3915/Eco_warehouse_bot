506e1f3908ff450f273e33cf758dfc8e88ccdb5b
Ebinezer <ebynjust@gmail.com>
2025-07-12 10:46:54 +0530

    initialise src folder

---
+++ src/eco_warehouse_bot/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'eco_warehouse_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/display.launch.py']),
+        ('share/' + package_name + '/urdf', ['urdf/eco_bot.xacro'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ebinezerjustin',
+    maintainer_email='ebynjust@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
